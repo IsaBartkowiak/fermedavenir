60918e809eb318219cb2993cb0cfa14b07da131f
Frederic Briolet <fredericbriolet@gmail.com>
2016-04-14 17:01:57 +0200

    Toggle carrot fixed #30

--- app/views/farms/tutorial.js
+++ app/views/farms/tutorial.js
@@ -1,2 +1,7 @@
 <% notice = @farm.tutorial ? "activés" : "désactivés" %>
-alert('Tutoriels <%= notice %>');
+$('.toggle-carrot').toggleClass('disabled');
+if( $('.toggle-carrot').hasClass('disabled')){
+	$('.toggle-carrot p').html('Aide désactivée<br/><i>Cliquez pour l\'activer</i>');
+} else {
+	$('.toggle-carrot p').html('Aide activée<br/><i>Cliquez pour la désactiver</i>');
+}
